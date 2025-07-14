a76ca68ba90ca99d11da4abfb95cf3202ac2785e
CarlosGalaron <carlos.galaron@gmail.com>
2025-07-14 21:13:39 +0200

    group assembly

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+require('dotenv').config();
+
 app.use(cors());
 app.use(express.json());
+
+const db = require('./config/db');
 
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hola desde el backend!' });
 });
 
-app.listen(3000, () => console.log('Backend en http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor backend en http://localhost:${PORT}`));
---
+++ frontend/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hola, LearnTech Expo App 🚀</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, justifyContent: 'center', alignItems: 'center',
+  },
+});
